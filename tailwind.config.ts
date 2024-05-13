800af05494b2a98d8166843ba0629467a89e7f6c
Abrão Rodrigues <arnf2710@gmail.com>
2024-05-13 13:25:27 -0300

    Adds mock data

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "governor-bay": {
+          "50": "#eef2ff",
+          "100": "#e0e7ff",
+          "200": "#c7d2fe",
+          "300": "#a6b4fb",
+          "400": "#828df7",
+          "500": "#6467f0",
+          "600": "#5047e4",
+          "700": "#4b41cb",
+          "800": "#3831a2",
+          "900": "#322f80",
+          "950": "#1e1b4b",
+        },
+        "accent-1": "#FFB149",
+        "accent-2": "#75D2FF",
+      },
     },
   },
   plugins: [],
